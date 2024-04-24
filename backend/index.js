b0be10e7d09f27374596262ac52e3a5800ea93ec
amansagar88 <amansagaraman8851@gmail.com>
2024-04-24 13:37:09 +0530

    some main changes

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,15 @@ import cors from  'cors';
 const app = express();
 app.use(cors(
     {
-        origin : ["https://ridebrowithbackend.vercel.app/"],
+        origin : ["https://ridebro.vercel.app/"],
         methods :["GET", "POST"] ,
         credentials : true
     }
 ));
 
 app.get("/", (req, res)=>{
-    // res.send("Hello World! from backend");
-    res.json("hello")
+    res.send("Hello World! from backend");
+    // res.json("hello")
 })
 
 app.listen(5000, ()=> console.log("App is running"));
